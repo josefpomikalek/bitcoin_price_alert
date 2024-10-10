6e88a058d7d59b0b10fc3a510845ebc1f25967c8
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-10 18:47:46 +0200

    Get the bitcoin price data in raw JSON text.

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from creds import ALPHA_VANTAGE_API_KEY
+
+CRYPTO_ENDPOINT = "https://www.alphavantage.co/query"
+CRYPTO = "BTC"
+FIAT = "USD"
+
+crypto_params = {
+    "function": "CURRENCY_EXCHANGE_RATE",
+    "from_currency": CRYPTO,
+    "to_currency": FIAT,
+    "apikey": ALPHA_VANTAGE_API_KEY
+}
+response = requests.get(CRYPTO_ENDPOINT, params=crypto_params)
+data = response.json()
+print(data)
