5263a558e03daa4112d93362b89492c9f2d43fde
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-14 22:35:49 +0200

    Send alert using Twilio if the condition is met.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
-from creds import ALPHA_VANTAGE_API_KEY, TWILIO_SID, TWILIO_AUTH_TOKEN
+from creds import ALPHA_VANTAGE_API_KEY, TWILIO_SID, TWILIO_AUTH_TOKEN, MY_TWILIO_TEST_PHONE_NUMBER, MY_PHONE_NUMBER
 from twilio.rest import Client
 
 CRYPTO_ENDPOINT = "https://www.alphavantage.co/query"
 CRYPTO = "BTC"
 FIAT = "USD"
-ALERT_PRICE = 62000
+ALERT_PRICE = 65000
 
 crypto_params = {
     "function": "CURRENCY_EXCHANGE_RATE",
@@ -26,3 +26,8 @@ if price > ALERT_PRICE:
 
     # Send alert to my phone number via Twilio
     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body=alert_text,
+        from_=MY_TWILIO_TEST_PHONE_NUMBER,
+        to=MY_PHONE_NUMBER
+    )
